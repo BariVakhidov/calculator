81a03329bb96c5dff9bee122ec72573d2b73cdce
bary <bari.vaxidov@mail.ru>
2021-03-15 04:31:49 +0300

    small fix

--- src/functions/functions.ts
+++ src/functions/functions.ts
@@ -3,7 +3,7 @@ export const getDataFromLocalStorage = ():Array<string | null> => {
 
         for (let i = 0; i < localStorage.length; i++) {
             let key:string|null = localStorage.key(i);
-            if (key !== null) {
+            if (key !== null && (localStorage.getItem(key) !== null) && key.search("operation") !== null) {
                 history.push(localStorage.getItem(key));
             }
         }
